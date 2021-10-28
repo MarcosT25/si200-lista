661adf8c4ec2692ff9ba4715b3d32be629fe946a
Marcos Takahashi <m254311@dac.unicamp.br>
2021-10-28 18:49:42 -0300

    Refactor: exercise 2 vowel list update

--- e2.c
+++ e2.c
@@ -8,14 +8,14 @@ void main() {
     fp = fopen("arquivo_base.txt", "r");
     char c;
     int count = 0;
-    char vowel_list[5] = {'a', 'e', 'i', 'o', 'u'};
-    int vowel_ascii[5];
-    for (int i = 0; i < 5; i++) {
+    char vowel_list[10] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    int vowel_ascii[10];
+    for (int i = 0; i < 10; i++) {
         vowel_ascii[i] = vowel_list[i];
     }
     while (!feof(fp)) {
         c = fgetc(fp);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             if (c == vowel_ascii[i]) {
                 count++;
                 break;
